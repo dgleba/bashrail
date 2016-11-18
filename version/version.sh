06c7d4b672f34c5845cc015a9c4489032cd6394f
David Gleba <dgleba@gmail.com>
2016-11-17 19:19:43 -0500

    mods

--- version/version.sh
+++ version/version.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 function git_func {
+    # I am not using this..
     GITBIN=git
     if [[ $1 == "commit" ]] && [[ "$#" -ne 1 ]]
     then
@@ -25,6 +26,8 @@ function git_func {
 function gitver {
   GITBIN=git
   folder=version
+  
+  # method 1
   if [[ 1 == 1 ]] 
   then
     if [ ! -f $folder/version-a.txt ] ; then
@@ -36,29 +39,33 @@ function gitver {
     perl -e "s/(\d+\.\d+\.)\d+/\${1}$GIT_VERSION/;" -pi.save $folder/version-a.txt
     #rm $folder/version-a.txt.save
 
-    echo "Version: $GIT_VERSION" > $folder/version-n.txt
+    # method 2
+    echo "Version(CountofCommits): $GIT_VERSION" > $folder/version-n.txt
   fi
 }
 
 
 function ver_dn {
 
-if [ ! -f version_date.txt ] ; then
-  echo 'Version, 2016, 21' > version_date.txt
-fi
+folder=version
+GIT_VERSION=`$GITBIN rev-list HEAD | wc -l`
+let GIT_VERSION+=1
 
+if [ ! -f $folder/version.txt ] ; then
+  echo 'Version(Date,CountCommits,IncrementingNumber), 2016, 14, 14' > $folder/version.txt
+fi
+    
 IFS=,
 while read ver DATE1 NUM
 do
-echo $ver, $(date +"%Y.%m.%d_%H.%M.%S"), $((NUM+1)) > tmpversiondatenumfile
-done < version_date.txt 
-mv tmpversiondatenumfile version_date.txt
-}
+echo $ver, $(date +"%Y.%m.%d_%H.%M.%S"),  $GIT_VERSION,  $((NUM+1)) > $folder/tmpversiondatenumfile
+done < $folder/version.txt 
 
+mv $folder/tmpversiondatenumfile $folder/version.txt
+}
 
 
 set -vx
 
 gitver
 ver_dn
-

aedff2e110522217e473eb03f5432e6705487edb
David Gleba <dgleba@gmail.com>
2016-11-27 00:20:18 -0500

    more devise

--- devise1.sh
+++ devise1.sh
@@ -4,8 +4,15 @@
  
  
 echo 'gem "devise"' >> Gemfile
+
+# https://github.com/hisea/devise-bootstrap-views
+echo 'gem "devise-bootstrap-views"' >> Gemfile
+
 #echo 'gem "devise_ldap_authenticatable", :git => "git://github.com/cschiewek/devise_ldap_authenticatable.git"' >> Gemfile
+
+
 bundle
+
 
 rails generate devise:install
 rails generate devise user
@@ -13,7 +20,11 @@ rake db:migrate
 
 #rails g devise_ldap_authenticatable:install
 
-rails generate devise:views 
+
+# rails generate devise:views 
+
+# for bootstrap views..
+rails g devise:views:locale en
 
 
 git add -A # Add all files and commit them
@@ -25,6 +36,28 @@ git commit -m "add devise 1"
 sed -i '/ApplicationController/a  before_filter :authenticate_user!' app/controllers/application_controller.rb
   
 
+  
+  
+#  error.. this puts in line1 in many many lines...
+
+# add line after match...  sed -i '/CLIENTSCRIPT="foo"/a CLIENTSCRIPT2="hello"' file
+#
+# file1='app/assets/stylesheets/application.scss'
+# pattern1='require_self'
+# line1=' \*= require devise_bootstrap_views'
+# sed  -i "0,/$pattern1/a $line1" $file1
+  
+# use ruby..
+  
+file1='app/assets/stylesheets/application.scss'
+# ruby -pi.bak.txt -e "gsub(/require_self/ , 'require_self\n *= require devise_bootstrap_views')" $file1
+ruby -pi.baktxt -e 'gsub(/require_self/ , "require_self\n *= require devise_bootstrap_views")' $file1
+mkdir -p _offline
+cp "$file1".baktxt _offline/application.scss."$(date +"__%Y.%m.%d_%H.%M.%S")".baktxt
+rm $file1.baktxt
+
+
+  
 cat << 'HEREDOC' >> db/seeds.rb
 
 User.create! email: 'a@e',  password: 'a', password_confirmation: 'a'

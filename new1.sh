4b92744d58b27ed4b003c69fcece8e19c4c3cae8
dgleba <dgleba@gmail.com>
2017-01-04 07:57:46 -0500

    install rails

--- new1.sh
+++ new1.sh
@@ -5,12 +5,14 @@
 # new rails app  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 pwd
 
+gem install rails -v 4.2.7.1 --no-ri --no-rdoc
+
 ruby -v ; rails -v
 
 
 if [  -d "$appn" ]; then
   # Control will enter here if DIRECTORY  exists.
-  set +vx 
+  set +vx
   echo 'Error 8!  - app name folder already exists.'
   #seco=110 ; read -t $seco -p "Hit ENTER or wait $seco seconds"
   exit 8
@@ -22,7 +24,7 @@ rails _4.2.7.1_ new $appn --skip-bundle
 
 if [ ! -d "$appn" ]; then
   # Control will enter here if DIRECTORY doesn't exist.
-  set +vx 
+  set +vx
   echo 'Error 9!  - app not created. folder doesn`t exist after attempting to create it.'
   #seco=110 ; read -t $seco -p "Hit ENTER or wait $seco seconds"
   exit 9
